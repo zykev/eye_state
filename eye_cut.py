62bc0fe11067692faa3ba1d0591479906ee04bd1
zeyuchen-kevin <zeyu.chen07@gmail.com>
2021-02-28 14:46:25 +0800

    initial commit

---
+++ eye_cut.py
@@ -0,0 +1,43 @@
+from facenet_pytorch import MTCNN
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import matplotlib.patches as patches
+import dlib
+
+
+image_path = 'E:/onlyfat_selfa3D_2/Data/Data-S235/1100/1.jpg'
+predictor_model_path = 'E:/onlyfat_selfa3D_2/face_detect_align/detector/shape_predictor_68_face_landmarks.dat'
+# Create face detector
+detector = MTCNN(select_largest=False, post_process=False)
+shape_predictor = dlib.shape_predictor(predictor_model_path)
+
+
+img = cv2.imread(image_path)
+#cv2.imshow('img',img)
+boxes, probs = detector.detect(img)
+boxes = boxes.squeeze(0)
+det = dlib.rectangle(int(boxes[0]), int(boxes[1]), int(boxes[2]), int(boxes[3]))
+face_landmarks = [(item.x, item.y) for item in shape_predictor(img, det).parts()]
+face_landmarks = np.array(face_landmarks)
+
+eye_legth = 1.6 * (face_landmarks[39,0] - face_landmarks[36,0])
+eye_width = 3.5 * (face_landmarks[41,1] - face_landmarks[37,1])
+eye_startx = face_landmarks[36,0] - 0.3 * eye_legth
+eye_starty = face_landmarks[37,1] - 0.3 * eye_width
+eye_img = img[int(eye_starty):int(eye_starty+eye_width), int(eye_startx):int(eye_startx+eye_legth)]
+cv2.imshow('img', eye_img)
+
+
+eye_img = cv2.resize(eye_img, (24,24))
+eye_img = cv2.cvtColor(eye_img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('img', eye_img)
+
+
+plt.figure(figsize=(12,8))
+plt.imshow(img)
+currentAxis=plt.gca()
+rect=patches.Rectangle((boxes[0], boxes[1]),boxes[2]-boxes[0],boxes[3]-boxes[1],linewidth=1,edgecolor='r',facecolor='none')
+currentAxis.add_patch(rect)
+plt.scatter(face_landmarks[:,0], face_landmarks[:,1])
+plt.show()
---
+++ training.py
@@ -0,0 +1,136 @@
+import torch.nn as nn
+import argparse
+import datetime
+from eye_state.network import LeNet5
+from eye_state.load_data import LoadEyeData
+from eye_state.util import *
+
+parser = argparse.ArgumentParser(description='Eye State Training')
+parser.add_argument('--epochs', default=50, type=int, help='number of total epochs to run (default: 50)')
+parser.add_argument('--lr', default=0.001, type=float, help='initial learning rate (default: 0.001)')
+parser.add_argument('--batch_size', default=32, type=int, help='number of data in one batch (default: 32)')
+
+args = parser.parse_args()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+ROOT_TRAIN = 'E:/onlyfat_selfa3D_2/Data/Eye_Images/train_info.csv'
+ROOT_TEST = 'E:/onlyfat_selfa3D_2/Data/Eye_Images/test_info.csv'
+DATA_DIR = 'E:/onlyfat_selfa3D_2/eye_state/Eye_Images/LeftEyes'
+min_loss = 10
+
+def main():
+    train_loader, test_loader = LoadEyeData(ROOT_TRAIN, ROOT_TEST, DATA_DIR, args.batch_size)
+    model = LeNet5().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    criterion = nn.CrossEntropyLoss()
+
+    # set objects for storing metrics
+    train_losses = []
+    valid_losses = []
+    train_acces = []
+    valid_acces = []
+
+    # Train model
+    for epoch in range(0, args.epochs):
+        # training
+        #train_loss, train_acc = train(train_loader, model, criterion, optimizer, device)
+        model, optimizer, train_loss, train_acc = train(train_loader, model, criterion, optimizer, device)
+        train_losses.append(train_loss.detach().cpu().item())
+        train_acces.append(train_acc.detach().cpu().item())
+
+        # validation
+        with torch.no_grad():
+            #valid_loss, valid_acc = validate(test_loader, model, criterion, device)
+            model, valid_loss, valid_acc = validate(test_loader, model, criterion, device)
+            valid_losses.append(valid_loss.detach().cpu().item())
+            valid_acces.append(valid_acc.detach().cpu().item())
+
+            print(f'{datetime.now().time().replace(microsecond=0)} --- '
+                  f'Epoch:      {epoch}\t'
+                  f'Train loss: {train_loss.detach().cpu().item():.4f}\t'
+                  f'Valid loss: {valid_loss.detach().cpu().item():.4f}\t'
+                  f'Train acc:  {100 * train_acc.detach().cpu().item():.2f}\t'
+                  f'Valid acc:  {100 * valid_acc.detach().cpu().item():.2f}')
+
+        # model save
+        is_better = valid_loss < min_loss
+        if is_better:
+            print('better model!')
+            min_loss = min(valid_loss, min_loss)
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'state_dict': model.state_dict(),
+                'loss': valid_loss,
+                'acc': valid_acc
+            })
+        else:
+            print('Model too bad & not save')
+
+    # training loss and acc plot
+    plot_training(train_losses, valid_losses, train_acces, valid_acces)
+
+
+def train(train_loader, model, criterion, optimizer, device):
+    '''
+    Function for the training step of the training loop
+    '''
+
+    model.train()
+    running_loss = 0
+    outputs = []
+    labels = []
+
+    for data, label in train_loader:
+        optimizer.zero_grad()
+
+        data = data.to(device)
+        label = label.to(device)
+
+        # Forward pass
+        output = model(data)
+        loss = criterion(output, label)
+        running_loss += loss.item() * data.size(0)
+        outputs.append(output)
+        labels.append(label)
+
+
+        # Backward pass
+        loss.backward()
+        optimizer.step()
+
+    epoch_loss = running_loss / len(train_loader.dataset)
+    epoch_acc = get_accuracy(outputs, labels)
+
+    return model, optimizer, epoch_loss, epoch_acc
+
+
+def validate(test_loader, model, criterion, device):
+    '''
+    Function for the validation step of the training loop
+    '''
+
+    model.eval()
+    running_loss = 0
+    outputs = []
+    labels = []
+
+
+    for data, label in test_loader:
+        data = data.to(device)
+        label = label.to(device)
+
+        # Forward pass and record loss
+        output = model(data)
+        loss = criterion(output, label)
+        running_loss += loss.item() * data.size(0)
+        outputs.append(output)
+        labels.append(label)
+
+    epoch_loss = running_loss / len(test_loader.dataset)
+    epoch_acc = get_accuracy(outputs, labels)
+
+    return model, epoch_loss, epoch_acc
+
+
+if __name__ == '__main__':
+    main()
